dfd164eb1a7b631e90e12b0e3da86806ffa4aa2d
David <lyokofirelyte@gmail.com>
2016-06-05 17:02:53 -0500

    rank change

--- glorp/com/github/lyokofirelyte/Wubalubadubdub/Data/WubRank.java
+++ glorp/com/github/lyokofirelyte/Wubalubadubdub/Data/WubRank.java
@@ -2,7 +2,7 @@ package com.github.lyokofirelyte.Wubalubadubdub.Data;
 
 public enum WubRank {
 
-	BARON(0, "1"),
+	/*BARON(0, "1"),
 	FREIHERR(1, "2"),
 	VISCOUNT(2, "3"),
 	COUNT(3, "4"),
@@ -15,7 +15,22 @@ public enum WubRank {
 	DUKE(10, "b"),
 	GRAND_PRINCE(11, "c"),
 	GRAND_DUKE(12, "d"),
-	ARCHDUKE(13, "e");
+	ARCHDUKE(13, "e");*/
+	
+	SHOCHI(0, "1"),
+	DAICHI(1, "2"),
+	SHOGI(2, "3"),
+	DAIGI(3, "4"),
+	SHOSHIN(4, "5"),
+	DAISHIN(5, "6"),
+	SHOREI(6, "7"),
+	DAIREI(7, "8"),
+	SHOJIN(8, "9"),
+	DAIJIN(9, "a"),
+	SHOTOKU(10, "b"),
+	DAITOKU(11, "c");
+	//GRAND_DUKE(12, "d"),
+	//ARCHDUKE(13, "e");
 	
 	WubRank(int rank, String color){
 		this.rank = rank;
